e86dd6d3c7fc6945d4a3d6c54d4cc022060d9f44
Yahir2023 <nohramirezalejandro@gmail.com>
2024-02-19 11:31:54 -0500

    endpoints

--- backend/src/controllers/producto.controller.ts
+++ backend/src/controllers/producto.controller.ts
@@ -1,5 +1,6 @@
-import { Producto } from "../models/producto";
+import { Producto, ProductoAttributes } from "../models/producto";
 import { Controller } from "../types";
+import { CrearProducto, EditarProducto } from "../types/producto";
 
 export const GetProductos : Controller<Producto[]> = async ( req , res ) => {
     
@@ -20,6 +21,113 @@ export const GetProductos : Controller<Producto[]> = async ( req , res ) => {
         console.log(err);
         
         return res.status(400).json()
+    }
+
+}
+
+export const CrearProductoC : Controller<ProductoAttributes, CrearProducto> = async ( req , res ) => {
+    
+    try
+    {
+        
+        const productoNuevo = await Producto.create({
+            ...req.body
+        })
+
+        return res.status(200).json({
+            ok: true,
+            data: productoNuevo,
+        })
+
+    }
+    catch(err)
+    {
+        
+        console.log(err);
+        
+        return res.status(400).json()
+    }
+
+}
+
+export const EditarProductoCtrl : Controller<ProductoAttributes | null, EditarProducto> = async ( req , res ) => {
+    
+    try
+    {
+        const { id , ...rest } = req.body
+
+        const productoAEditar = await Producto.findOne({
+            where: { id }
+        })
+
+        if( !productoAEditar )
+        {
+            return res.status(400).json({
+                ok: false,
+                msg: "Producto invalido",
+                data: null,
+            })
+        }
+
+        productoAEditar.set({
+            ...rest
+        })
+
+        await productoAEditar.save()
+
+        return res.status(200).json({
+            ok: true,
+            data: productoAEditar,
+            msg: "Producto actualizado correctamente"
+        })
+
+    }
+    catch(err)
+    {
+        
+        console.log(err);
+        
+        return res.status(400).json()
+    }
+
+}
+
+export const EliminarProductoCtrl : Controller<string | null, number , any , { id: string }> = async ( req , res ) => {
+    
+    try
+    {
+        const { id } = req.params
+
+        const productoAEliminar = await Producto.findOne({
+            where: { id }
+        })
+
+        if( !productoAEliminar )
+        {
+            return res.status(400).json({
+                ok: false,
+                msg: "Producto invalido",
+                data: null,
+            })
+        }
+
+        await productoAEliminar.destroy()
+
+
+        return res.status(200).json({
+            ok: true,
+            data: null,
+            msg: "Producto eliminado correctamente"
+        })
+
+    }
+    catch(err)
+    {
+        
+        console.log(err);
+        
+        return res.status(400).json()
+        
     }
 
 }
--- backend/src/router/producto.routes.ts
+++ backend/src/router/producto.routes.ts
@@ -1,8 +1,14 @@
-import { GetProductos } from '../controllers/producto.controller';
+import { CrearProductoC, EditarProductoCtrl, EliminarProductoCtrl, GetProductos } from '../controllers/producto.controller';
 import { Router } from 'express';
 
 const router = Router();
 
-router.get( '/', GetProductos );
+router.get( '/all', GetProductos );
+
+router.post( '/create', CrearProductoC );
+
+router.put( '/edit', EditarProductoCtrl );
+
+router.delete( '/:id', EliminarProductoCtrl );
 
 export default router;
