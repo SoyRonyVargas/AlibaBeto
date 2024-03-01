578a4a69132f12be4a53eb66f6cdeb2e6925f741
Betopusey <beto>
2024-03-01 10:10:54 -0500

    Add navbas beto

--- alibabeto-fronted/astro.config.mjs
+++ alibabeto-fronted/astro.config.mjs
@@ -1,8 +1,8 @@
-import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
   // ...
   integrations: [tailwind()],
-  output: 'server'
+  output: "server",
 });
