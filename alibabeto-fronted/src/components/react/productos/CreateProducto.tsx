9f17956188645b6bb016ebf99693cff2860c0169
Rony Vargas <47135307+SoyRonyVargas@users.noreply.github.com>
2024-03-13 10:48:42 -0600

    feat: correccion formulario

--- alibabeto-fronted/src/components/react/productos/CreateProducto.tsx
+++ alibabeto-fronted/src/components/react/productos/CreateProducto.tsx
@@ -39,7 +39,7 @@ const CreateProducto = () => {
                     JSON.stringify(formState, null, 3)
                 }
             </pre> */}
-            <div className="max-w-2xl mx-auto bg-white p">
+            <div className="p-7 rounded-lg mx-auto bg-white border">
                 <form onSubmit={handleSubmitForm}>
                     <div className="grid gap-6 mb-6 lg:grid-cols-1">
                         <div>
@@ -53,6 +53,22 @@ const CreateProducto = () => {
                                 type="text"
                                 className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                                 placeholder="CELULAR"
+                                name='codigo'
+                                value={formState.codigo}
+                                onChange={onInputChange}
+                            />
+                        </div>
+                        <div>
+                            <label
+                                htmlFor="first_name"
+                                className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+                            >
+                                Titulo
+                            </label>
+                            <input
+                                type="text"
+                                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                placeholder=""
                                 name='codigo'
                                 value={formState.codigo}
                                 onChange={onInputChange}
@@ -134,7 +150,7 @@ const CreateProducto = () => {
                     </div> */}
                     <button
                         type="submit"
-                        className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                        className="text-white w-full bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-lg font-bold px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
                     >
                         Crear
                     </button>
